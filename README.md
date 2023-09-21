108d41b27ed10220853423fa2b49b2704dfa2c69
faraz2023 <faraz.khoshbakhtian@mail.utoronto.ca>
2023-09-21 13:45:02 -0400

    added two sum code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cpp_practices
 A personal repo where I keep my cpp practices; mostly from leet code
+
+
+Rudimentary stuff:
+
+To compile a cpp file, run the following command:
+
+`g++ main.cpp -o main`
+
+To run the compiled file, run the following command:
+
+`./main`
---
+++ easy/two_sum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            cout << complement << '\n'; // Added space for better readability
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = sol.twoSum(nums, target);
+    
+    if (!result.empty()) {
+        cout << "Indices are: " << result[0] << ' ' << result[1] << '\n';
+    } else {
+        cout << "No two sum solution\n";
+    }
+
+    return 0;
+}
